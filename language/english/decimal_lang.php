fc79c53bfaa4367ff9af96f4fb380b818a8febc6
Ryan Thompson <service@aiwebsystems.com>
2013-08-08 02:52:30 -0500

    Initial Commit

---
+++ language/english/decimal_lang.php
@@ -0,0 +1,6 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams:decimal.name'] 						= 'Decimal';
+$lang['streams:decimal.decimal_places']				= 'Decimal Places';
+$lang['streams:decimal.min_value']					= 'Min Value';
+$lang['streams:decimal.max_value']					= 'Max Value';
